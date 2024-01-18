465d62eafeeaa5b1f611df249db5fd92845b4533
oscrowd <oscrowd@gmail.com>
2024-01-18 11:21:46 +0300

    D-01.003.05

---
+++ TagsPractica.API/Controllers/PostsController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using TagsPractica.DAL;
+using TagsPractica.DAL.Models;
+using TagsPractica.API.ViewModels;
+
+
+
+namespace TagsPractica.API.Controllers
+
+{
+    [ApiController]
+    [Route("Controller")]
+    public class PostsController : Controller
+    {
+        private readonly DatabaseContext _context;
+
+        public PostsController(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Posts
+        [HttpGet]
+        public async Task<IActionResult> Posts()
+        {
+            if (_context.Posts != null)
+            {
+                return StatusCode(200, "Entity set 'DatabaseContext.Posts'  is null.");
+            }
+            else
+            {
+                var PostList = await _context.Posts.Include(m => m.Tags).ToListAsync();
+                return StatusCode(200, "dfsdfsdf");
+            }
+        }
+    }
+}
---
+++ TagsPractica.API/Controllers/TagsController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using TagsPractica.DAL;
+using TagsPractica.DAL.Models;
+using TagsPractica.API.ViewModels;
+
+namespace TagsPractica.API.Controllers
+{
+    [ApiController]
+    [Route("Controller")]
+    public class TagsController : Controller
+    {
+
+        private readonly DatabaseContext _context;
+
+        public TagsController(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Tags
+        [HttpGet]
+        public async Task<IActionResult> Index2()
+        {
+            if (_context.Tags != null)
+            {
+                return StatusCode(200, "Entity set 'DatabaseContext.Tags'  is null.");
+            }
+            else
+            {
+                var TagsTemp = await _context.Tags.ToListAsync();
+                //return StatusCode(200, TagsTemp);
+                return StatusCode(200, "sdas");
+            }
+        }
+
+        // GET: Tags/Details/5
+    }
+}
